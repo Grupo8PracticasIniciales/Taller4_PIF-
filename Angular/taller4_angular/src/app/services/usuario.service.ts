998f6299905aa423f1b0c45bdb0d6c1e2b0fe356
Franklin ONP <3110022770701@ingenieria.usac.edu.gt>
2023-09-12 23:14:29 -0600

    frontend
    Se realizo el frontend de registrar usuario

--- Angular/taller4_angular/src/app/services/usuario.service.ts
+++ Angular/taller4_angular/src/app/services/usuario.service.ts
@@ -22,6 +22,7 @@ export class UsuarioService {
     return this._http.put(this.url+'/usuario/login',params,{headers: this.headersVariable})
   }
 
+
   getIdentidad(){
     var identidad2 = JSON.parse(localStorage.getItem('identidad') || '{}');
     if(identidad2 != 'undefined'){
